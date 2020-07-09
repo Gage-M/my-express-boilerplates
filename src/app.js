405ed661eca96c3fc0270928e4888456eee9e3d5
Gage <gagemigan@gmail.com>
2020-07-09 13:02:52 -0400

    typo fix &  other

--- src/app.js
+++ src/app.js
@@ -17,17 +17,19 @@ app.use(cors());
 
 
 app.get( '/', (req,res) => {
+//   throw new Error('Error makes computer fans go brrrr');
   res.status(200).send('OwO wi mwaking gwod pwa gwas!');
 });
 
-app.use(function errorHandler(error, req , res , next){
+app.use(function errorHandler(error, req , res , next){/*eslint-disable-line*/
   let response;
   if ( NODE_ENV === 'production'){
     response = { error : {message : 'server error' } };
   }else{
-    console.error(error);
+    console.error(error);/*eslint-disable-line*/
     response = { message : error.message, error };
   }
+  res.status(500).json(response).send();
 });
 
 
